6c02b75a7bd6b400a28220acbc2dc71471b9c618
aglegaspi <lex83nyc@gmail.com>
2019-10-21 11:49:38 -0400

    set up Settings View Controller

--- iOS_NYTimesBestseller_App/Controllers/SettingsVC.swift
+++ iOS_NYTimesBestseller_App/Controllers/SettingsVC.swift
@@ -10,21 +10,81 @@ import UIKit
 
 class SettingsVC: UIViewController {
 
+    //var categories: Categories!
+    let pickerData = ["11", "12", "13"]
+    
+    //MARK: VIEWS
+    var settingsLabel: UILabel = {
+        var label = UILabel()
+        label.text = "Pick Default Category"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    var categoryPicker: UIPickerView = {
+        var picker = UIPickerView()
+    
+        
+        return picker
+    }()
+    
+    //MARK: LIFECYCLES
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        loadViews()
+        loadContraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: LOADERS
+    private func loadViews() {
+        view.addSubview(settingsLabel)
+        view.addSubview(categoryPicker)
+        categoryPicker.delegate = self
+        categoryPicker.dataSource = self
     }
-    */
+    
+    //MARK: PRIVATE FUNCTIONS
+    private func loadContraints() {
+        settingsLabelConstraints()
+        categoryPickerConstraints()
+    }
+    
+    private func settingsLabelConstraints() {
+      settingsLabel.translatesAutoresizingMaskIntoConstraints = false
+      NSLayoutConstraint.activate([
+       settingsLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+        settingsLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+        settingsLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+        settingsLabel.heightAnchor.constraint(equalToConstant: 30)
+      ])
+    }
+    
+    private func categoryPickerConstraints() {
+      categoryPicker.translatesAutoresizingMaskIntoConstraints = false
+      NSLayoutConstraint.activate([
+        categoryPicker.topAnchor.constraint(equalTo: settingsLabel.bottomAnchor),
+        categoryPicker.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+        categoryPicker.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+        categoryPicker.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+      ])
+    }
 
+
+}
+
+extension SettingsVC: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    
 }
